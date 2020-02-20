e670ec8c8704afcc174ba9a1cd01ddec06525b06
Thomas McInnes <tommcinnes1@outlook.com>
2020-02-20 17:13:42 -0500

    UPPERFACE
    *uppercase.  But whatever I have a headache.  Added support for uppercase letters, uses special characters only for certain books now bc the compute time was getting ridiculous.

--- Generator.R
+++ Generator.R
@@ -6,10 +6,11 @@ books <- unique(rawb$book)
 master <- c()
 years <- 1800:2200
 sepchars_y <- c('@', '!', '$', '-', '.', ':', '_')
-sepchars <- c('-', '_', ':', '.')
+sepchars <- c('_', '.', '-', '!', '@', ':')
+favs <- c('Genesis', 'Exodus', 'Job', 'Psalm', 'Proverbs', 'Matthew', 'Mark', 'Luke', 'John', 'Romans', '1 Corinthians', 'Acts', 'Revelation')
 list3 <- c()
 listS <- c()
-list2 <- c('jesus', 'Jesus', 'jesus!', 'Jesus!')
+list2 <- c('jesus', 'Jesus', 'jesus!', 'Jesus!', 'JESUS', 'JESUS!')
 for (i in 1:length(books)){
   b <- books[i]
   setb <- rawb[rawb$book == b,]
@@ -20,11 +21,15 @@ for (i in 1:length(books)){
     list3 <- c(list3, paste(b, toString(y), '!', sep=''))
     list3 <- c(list3, paste(tolower(b), toString(y), sep=''))
     list3 <- c(list3, paste(tolower(b), toString(y), '!', sep=''))
+    list3 <- c(list3, paste(toupper(b), toString(y), sep=''))
+    list3 <- c(list3, paste(toupper(b), toString(y), '!', sep=''))
     for (sc in sepchars_y) {
       list3 <- c(list3, paste(b, sc, toString(y), sep=''))
       list3 <- c(list3, paste(b, sc, toString(y), '!', sep=''))
       list3 <- c(list3, paste(tolower(b), sc, toString(y), sep=''))
       list3 <- c(list3, paste(tolower(b), sc, toString(y), '!', sep=''))
+      list3 <- c(list3, paste(toupper(b), sc, toString(y), sep=''))
+      list3 <- c(list3, paste(toupper(b), sc, toString(y), '!', sep=''))
     }
   }
   if (b == 'Revelation' || b == 'Psalm') {
@@ -34,11 +39,15 @@ for (i in 1:length(books)){
       list3 <- c(list3, paste(b4, toString(y), '!', sep=''))
       list3 <- c(list3, paste(tolower(b4), toString(y), sep=''))
       list3 <- c(list3, paste(tolower(b4), toString(y), '!', sep=''))
+      list3 <- c(list3, paste(toupper(b4), toString(y), sep=''))
+      list3 <- c(list3, paste(toupper(b4), toString(y), '!', sep=''))
       for (sc in sepchars_y) {
         list3 <- c(list3, paste(b4, sc, toString(y), sep=''))
         list3 <- c(list3, paste(b4, sc, toString(y), '!', sep=''))
         list3 <- c(list3, paste(tolower(b4), sc, toString(y), sep=''))
         list3 <- c(list3, paste(tolower(b4), sc, toString(y), '!', sep=''))
+        list3 <- c(list3, paste(toupper(b4), sc, toString(y), sep=''))
+        list3 <- c(list3, paste(toupper(b4), sc, toString(y), '!', sep=''))
       }
     }
   }
@@ -47,11 +56,15 @@ for (i in 1:length(books)){
     list <- c(list, paste(b, toString(c), '!', sep=''))
     list <- c(list, paste(tolower(b), toString(c), sep=''))
     list <- c(list, paste(tolower(b), toString(c), '!', sep=''))
+    list <- c(list, paste(toupper(b), toString(c), sep=''))
+    list <- c(list, paste(toupper(b), toString(c), '!', sep=''))
     for (sc in sepchars) {
       listS <- c(listS, paste(b, sc, toString(c), sep=''))
       listS <- c(listS, paste(b, sc, toString(c), '!', sep=''))
       listS <- c(listS, paste(tolower(b), sc, toString(c), sep=''))
       listS <- c(listS, paste(tolower(b), sc, toString(c), '!', sep=''))
+      listS <- c(listS, paste(toupper(b), sc, toString(c), sep=''))
+      listS <- c(listS, paste(toupper(b), sc, toString(c), '!', sep=''))
     }
     for (v in setb[setb$chapter == c, 'verse']) {
       list <- c(list, paste(b, toString(c), toString(v), sep=''))
@@ -62,24 +75,38 @@ for (i in 1:length(books)){
       list <- c(list, paste(tolower(b), toString(c), ':', toString(v), sep=''))
       list <- c(list, paste(tolower(b), toString(c), toString(v), '!', sep=''))
       list <- c(list, paste(tolower(b), toString(c), ':', toString(v), '!', sep=''))
-      for (sc in sepchars) {
-        listS <- c(listS, paste(b, sc, toString(c), toString(v), sep=''))
-        listS <- c(listS, paste(b, sc, toString(c), ':', toString(v), sep=''))
-        listS <- c(listS, paste(b, sc, toString(c), toString(v), '!', sep=''))
-        listS <- c(listS, paste(b, sc, toString(c), ':', toString(v), '!', sep=''))
-        listS <- c(listS, paste(tolower(b), sc, toString(c), toString(v), sep=''))
-        listS <- c(listS, paste(tolower(b), sc, toString(c), ':', toString(v), sep=''))
-        listS <- c(listS, paste(tolower(b), sc, toString(c), toString(v), '!', sep=''))
-        listS <- c(listS, paste(tolower(b), sc, toString(c), ':', toString(v), '!', sep=''))
+      list <- c(list, paste(toupper(b), toString(c), toString(v), sep=''))
+      list <- c(list, paste(toupper(b), toString(c), ':', toString(v), sep=''))
+      list <- c(list, paste(toupper(b), toString(c), toString(v), '!', sep=''))
+      list <- c(list, paste(toupper(b), toString(c), ':', toString(v), '!', sep=''))
+      if (b %in% favs) {
+        for (sc in sepchars) {
+          listS <- c(listS, paste(b, sc, toString(c), toString(v), sep=''))
+          listS <- c(listS, paste(b, sc, toString(c), ':', toString(v), sep=''))
+          listS <- c(listS, paste(b, sc, toString(c), toString(v), '!', sep=''))
+          listS <- c(listS, paste(b, sc, toString(c), ':', toString(v), '!', sep=''))
+          listS <- c(listS, paste(tolower(b), sc, toString(c), toString(v), sep=''))
+          listS <- c(listS, paste(tolower(b), sc, toString(c), ':', toString(v), sep=''))
+          listS <- c(listS, paste(tolower(b), sc, toString(c), toString(v), '!', sep=''))
+          listS <- c(listS, paste(tolower(b), sc, toString(c), ':', toString(v), '!', sep=''))
+          listS <- c(listS, paste(toupper(b), sc, toString(c), toString(v), sep=''))
+          listS <- c(listS, paste(toupper(b), sc, toString(c), ':', toString(v), sep=''))
+          listS <- c(listS, paste(toupper(b), sc, toString(c), toString(v), '!', sep=''))
+          listS <- c(listS, paste(toupper(b), sc, toString(c), ':', toString(v), '!', sep=''))
+        }
       }
       list2 <- c(list2, paste('jesus', toString(c), toString(v), sep=''))
       list2 <- c(list2, paste('jesus', toString(c), ':', toString(v), sep=''))
       list2 <- c(list2, paste('Jesus', toString(c), toString(v), sep=''))
       list2 <- c(list2, paste('Jesus', toString(c), ':', toString(v), sep=''))
+      list2 <- c(list2, paste('JESUS', toString(c), toString(v), sep=''))
+      list2 <- c(list2, paste('JESUS', toString(c), ':', toString(v), sep=''))
       list2 <- c(list2, paste('jesus', toString(c), toString(v), '!', sep=''))
       list2 <- c(list2, paste('jesus', toString(c), ':', toString(v), '!', sep=''))
       list2 <- c(list2, paste('Jesus', toString(c), toString(v), '!', sep=''))
       list2 <- c(list2, paste('Jesus', toString(c), ':', toString(v), '!', sep=''))
+      list2 <- c(list2, paste('JESUS', toString(c), toString(v), '!', sep=''))
+      list2 <- c(list2, paste('JESUS', toString(c), ':', toString(v), '!', sep=''))
       if (grepl('\\d', b)) {
         b3 <- gsub('1', 'I', b)
         b3 <- gsub('2', 'II', b3)
@@ -92,15 +119,25 @@ for (i in 1:length(books)){
         list <- c(list, paste(tolower(b3), toString(c), ':', toString(v), sep=''))
         list <- c(list, paste(tolower(b3), toString(c), toString(v), '!', sep=''))
         list <- c(list, paste(tolower(b3), toString(c), ':', toString(v), '!', sep=''))
-        for (sc in sepchars) {
-          listS <- c(listS, paste(b3, sc, toString(c), toString(v), sep=''))
-          listS <- c(listS, paste(b3, sc, toString(c), ':', toString(v), sep=''))
-          listS <- c(listS, paste(b3, sc, toString(c), toString(v), '!', sep=''))
-          listS <- c(listS, paste(b3, sc, toString(c), ':', toString(v), '!', sep=''))
-          listS <- c(listS, paste(tolower(b3), sc, toString(c), toString(v), sep=''))
-          listS <- c(listS, paste(tolower(b3), sc, toString(c), ':', toString(v), sep=''))
-          listS <- c(listS, paste(tolower(b3), sc, toString(c), toString(v), '!', sep=''))
-          listS <- c(listS, paste(tolower(b3), sc, toString(c), ':', toString(v), '!', sep=''))
+        list <- c(list, paste(toupper(b3), toString(c), toString(v), sep=''))
+        list <- c(list, paste(toupper(b3), toString(c), ':', toString(v), sep=''))
+        list <- c(list, paste(toupper(b3), toString(c), toString(v), '!', sep=''))
+        list <- c(list, paste(toupper(b3), toString(c), ':', toString(v), '!', sep=''))
+        if (b %in% favs) {
+          for (sc in sepchars) {
+            listS <- c(listS, paste(b3, sc, toString(c), toString(v), sep=''))
+            listS <- c(listS, paste(b3, sc, toString(c), ':', toString(v), sep=''))
+            listS <- c(listS, paste(b3, sc, toString(c), toString(v), '!', sep=''))
+            listS <- c(listS, paste(b3, sc, toString(c), ':', toString(v), '!', sep=''))
+            listS <- c(listS, paste(tolower(b3), sc, toString(c), toString(v), sep=''))
+            listS <- c(listS, paste(tolower(b3), sc, toString(c), ':', toString(v), sep=''))
+            listS <- c(listS, paste(tolower(b3), sc, toString(c), toString(v), '!', sep=''))
+            listS <- c(listS, paste(tolower(b3), sc, toString(c), ':', toString(v), '!', sep=''))
+            listS <- c(listS, paste(toupper(b3), sc, toString(c), toString(v), sep=''))
+            listS <- c(listS, paste(toupper(b3), sc, toString(c), ':', toString(v), sep=''))
+            listS <- c(listS, paste(toupper(b3), sc, toString(c), toString(v), '!', sep=''))
+            listS <- c(listS, paste(toupper(b3), sc, toString(c), ':', toString(v), '!', sep=''))
+          }
         }
       }
       if (b == 'Revelation' || b == 'Psalm') {
@@ -114,6 +151,10 @@ for (i in 1:length(books)){
         list <- c(list, paste(tolower(b2), toString(c), ':', toString(v), sep=''))
         list <- c(list, paste(tolower(b2), toString(c), toString(v), '!', sep=''))
         list <- c(list, paste(tolower(b2), toString(c), ':', toString(v), '!', sep=''))
+        list <- c(list, paste(toupper(b2), toString(c), toString(v), sep=''))
+        list <- c(list, paste(toupper(b2), toString(c), ':', toString(v), sep=''))
+        list <- c(list, paste(toupper(b2), toString(c), toString(v), '!', sep=''))
+        list <- c(list, paste(toupper(b2), toString(c), ':', toString(v), '!', sep=''))
         for (sc in sepchars) {
           listS <- c(listS, paste(b2, sc, toString(c), toString(v), sep=''))
           listS <- c(listS, paste(b2, sc, toString(c), ':', toString(v), sep=''))
@@ -123,6 +164,10 @@ for (i in 1:length(books)){
           listS <- c(listS, paste(tolower(b2), sc, toString(c), ':', toString(v), sep=''))
           listS <- c(listS, paste(tolower(b2), sc, toString(c), toString(v), '!', sep=''))
           listS <- c(listS, paste(tolower(b2), sc, toString(c), ':', toString(v), '!', sep=''))
+          listS <- c(listS, paste(toupper(b2), sc, toString(c), toString(v), sep=''))
+          listS <- c(listS, paste(toupper(b2), sc, toString(c), ':', toString(v), sep=''))
+          listS <- c(listS, paste(toupper(b2), sc, toString(c), toString(v), '!', sep=''))
+          listS <- c(listS, paste(toupper(b2), sc, toString(c), ':', toString(v), '!', sep=''))
         }
       }
     }
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # BiblePass
-Generates wordlists of possible passwords based off of Bible verses (e.g. john316)
+Generates wordlists of possible passwords based off of Bible verses (e.g. john316, Ephesians2:8, Revelation@2012, etc.)
+
+## Design
+* Generates different variations of book-chapter-verse, Book Capitalized/not capitalized/ALL CAPS
+* Generates list of book + year
+* Generates different variations of book-separating character-chapter-verse, also C/nc/AC
+* Also tries singular and plural forms for Revelation/Revelations and Psalm/Psalms
+
+## Output
+* ./Lists/<book of the Bible>.txt - Simple wordlist from each book of the Bible
+* ./Lists/Jesus.txt - Variations C/nc/AC of "jesus" combined with bible chapter/verse numbers
+* ./Lists/Years.txt - Books of the Bible + years (1800-2200) w/ and wo/ separating characters
+* ./Lists/SpecialMaster.txt - Master list of all books of the Bible with special characters
+* ./Master.txt - All of the above
